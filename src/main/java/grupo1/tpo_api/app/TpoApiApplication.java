f9c5d6915c41f77ea3de94ca88c6ff1bdd0fc326
GCamin <gian-1998@hotmail.com>
2023-09-02 04:28:27 -0300

    Initial commit

---
+++ src/main/java/grupo1/tpo_api/app/TpoApiApplication.java
@@ -0,0 +1,14 @@
+package grupo1.tpo_api.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpoApiApplication.class, args);
+		System.out.println("hola");
+	}
+
+}
---
+++ src/test/java/grupo1/tpo_api/app/TpoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo1.tpo_api.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
