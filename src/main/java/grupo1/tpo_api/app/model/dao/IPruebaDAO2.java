eb9de39f8131487d5310c917dd66cdfbe5095df5
MissingNno <fdesimone@uade.edu.ar>
2023-09-10 19:50:38 -0300

    lcdtm

--- src/main/java/grupo1/tpo_api/app/model/dao/IPruebaDAO2.java
+++
@@ -1,9 +0,0 @@
-package grupo1.tpo_api.app.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import grupo1.tpo_api.app.model.entity.prueba;
-
-public interface IPruebaDAO2 extends CrudRepository<prueba, Integer>{
-
-}
