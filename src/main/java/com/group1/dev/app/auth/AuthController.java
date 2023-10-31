bcfd44bc13c75fb65836cfb0fc6963e787d2a2be
ggenna88 <gonzalogenna@gmail.com>
2023-10-31 18:47:00 -0300

    CORS en AuthController

--- src/main/java/com/group1/dev/app/auth/AuthController.java
+++ src/main/java/com/group1/dev/app/auth/AuthController.java
@@ -2,11 +2,13 @@ package com.group1.dev.app.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
